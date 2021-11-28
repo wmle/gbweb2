aacc9c734e750664508249b8cf1baefaca3d2073
wmle <on.lock@gmail.com>
2021-11-28 03:29:19 +0300

    test update 28.11.2021

--- js/app.js
+++ js/app.js
@@ -1,12 +1,37 @@
 const buttonOpenNav = document.querySelectorAll('.header__item-button');
 const buttonCloseNav = document.querySelector('.main__menu-close');
-const menuNav = document.getElementsByClassName('main__menu')[0];
+const menuNav = document.querySelector('.main__menu');
 
-buttonOpenNav[1].onclick = () => menuNav.style.display = 'block';
-buttonCloseNav.onclick = () => menuNav.style.display = 'none';
+// buttonOpenNav[1].onclick = () => menuNav.classList.toggle('main__menu-active');
+// buttonCloseNav.onclick = () => menuNav.classList.remove('main__menu-active');
+
+// buttonOpenNav[1].onclick = () => menuNav.classList.toggle('main-active');
+
+
+buttonOpenNav[1].onclick = () => {
+    menuNav.classList.remove('main-close');
+    menuNav.classList.add('main-active');
+}
+buttonCloseNav.onclick = () => {
+    menuNav.classList.remove('main-active');
+    menuNav.classList.add('main-close');
+}
+
+
+// buttonOpenNav[1].onclick = () => {
+//     menuNav.classList.remove('main-close');
+//     // menuNav.style.display = 'block';
+//     menuNav.classList.add('main-active');
+// }
+// buttonCloseNav.onclick = () => {
+//     menuNav.classList.remove('main-active');
+//     menuNav.classList.add('main-close');
+// }
 
 
 
+// buttonOpenNav[1].onclick = () => menuNav.style.display = 'block';
+// buttonCloseNav.onclick = () => menuNav.style.display = 'none';
 
 
 // buttonOpenNav[1].onclick = () => menuNav.classList.add('main__menu-open');
