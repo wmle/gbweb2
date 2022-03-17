267b27dad4c78dd8212de9a82345b88fa71dd825
wmle <on.lock@gmail.com>
2022-03-18 01:20:37 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
+"use strict";
+
 const buttonOpenNav = document.querySelectorAll('.header__item-button');
 const buttonCloseNav = document.querySelector('.main__menu-close');
 const menuNav = document.querySelector('.main__menu');
 
-buttonOpenNav[1].onclick = () => {
+buttonOpenNav[1].addEventListener('click', () => {
     menuNav.classList.remove('main-close');
     menuNav.classList.add('main-active');
-}
-buttonCloseNav.onclick = () => {
+});
+buttonCloseNav.addEventListener('click', () => {
     menuNav.classList.remove('main-active');
     menuNav.classList.add('main-close');
-}
+});
+
 
 // buttonOpenNav[1].onclick = () => menuNav.classList.toggle('main__menu-active');
 // buttonCloseNav.onclick = () => menuNav.classList.remove('main__menu-active');
